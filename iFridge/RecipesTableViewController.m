0d6eb789b4e55a71458c0d58fef6ca4163fe72ff
AlexeyPelekh <apelex93@gmail.com>
2015-05-15 12:33:57 +0300

    iOS

---
+++ iFridge/RecipesTableViewController.m
@@ -0,0 +1,144 @@
+//
+//  RecipesTableViewController.m
+//  iFridge
+//
+//  Created by Alexey Pelekh on 5/14/15.
+//  Copyright (c) 2015 Alexey Pelekh. All rights reserved.
+//
+
+#import "RecipesTableViewController.h"
+#import "TableViewCellRecipes.h"
+
+
+@interface RecipesTableViewController ()
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+@end
+
+@implementation RecipesTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    [manager GET:@"https://api.edamam.com/search?q=meat&app_id=098aa935&app_key=e6f6e485b0222cf1b48439a164562270" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        self.allRecipes = (NSDictionary *) responseObject;
+        self.recipes = self.allRecipes[@"hits"];
+       NSLog(@"JSON: %@", self.recipes);
+//        self.name = self.allRecipes[@"orgTypes"];
+//        
+//        self.cookingTime = self.allRecipes[@"currencies"];
+//       
+//        self.calories = self.allRecipes[@"regions"];
+//      
+//        self.totalWeight = self.allRecipes[@"cities"];
+//        
+//        self.fat = self.allRecipes[@"orgTypes"];
+//        
+//        self.sugars = self.allRecipes[@"currencies"];
+//        
+//        self.cookingLevel = self.allRecipes[@"regions"];
+//        //NSLog(@"JSON: %@", self.regions);
+
+        [self.tableView reloadData];
+        
+        //NSLog(@"JSON: %@", self.post[0][@"address"]);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+    }];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+   
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+   
+
+   
+   return self.recipes.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    RecipesCell *cell = [tableView dequeueReusableCellWithIdentifier:@"myCell" forIndexPath:indexPath];
+    //        self.name = self.allRecipes[@"orgTypes"];
+    //
+    //        self.cookingTime = self.allRecipes[@"currencies"];
+    //
+    //        self.calories = self.allRecipes[@"regions"];
+    //
+    //        self.totalWeight = self.allRecipes[@"cities"];
+    //
+    //        self.fat = self.allRecipes[@"orgTypes"];
+    //
+    //        self.sugars = self.allRecipes[@"currencies"];
+    //
+    //        self.cookingLevel = self.allRecipes[@"regions"];
+    //        //NSLog(@"JSON: %@", self.regions);
+
+    cell.nameOfDish.text = self.recipes[indexPath.row][@"recipe"][@"label"];
+    cell.cookingLevel.text = self.recipes[indexPath.row][@"recipe"][@"level"];
+    cell.cookingTime.text = [NSString stringWithFormat:@"%@", self.recipes[indexPath.row][@"recipe"][@"cookingTime"]];
+//    cell.caloriesTotal.text = self.recipes[indexPath.row][@"recipe"][@"label"];
+//    cell.sugarsTotal.text = self.recipes[indexPath.row][@"recipe"][@"label"];
+//    cell.weightTotal.text = self.recipes[indexPath.row][@"recipe"][@"label"];
+//    cell.fatTotal.text = self.recipes[indexPath.row][@"recipe"][@"label"];
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
